0d1afe39efd6699eb409e4682b2470ec2e9a2040
Pierre Waechter <pierre.waechter@audencia.com>
2020-11-02 23:06:19 +0100

    new db

--- db/migrate/20201102212533_create_meals.rb
+++ db/migrate/20201102212533_create_meals.rb
@@ -1,9 +1,11 @@
 class CreateMeals < ActiveRecord::Migration[6.0]
   def change
     create_table :meals do |t|
-      t.references :cook, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
       t.float :price
       t.string :name
+      t.boolean :deleted
+      t.string :description
 
       t.timestamps
     end
---
+++ db/migrate/20201102212717_create_suborders.rb
@@ -0,0 +1,11 @@
+class CreateSuborders < ActiveRecord::Migration[6.0]
+  def change
+    create_table :suborders do |t|
+      t.references :meal, null: false, foreign_key: true
+      t.references :basket, null: false, foreign_key: true
+      t.integer :quantity
+
+      t.timestamps
+    end
+  end
+end
